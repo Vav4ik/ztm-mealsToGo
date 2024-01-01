24e4c71c880ce2ca8fd79158b9dcea018800e030
Vav4ik <wawaysms@gmail.com>
2024-01-01 19:16:31 +0000

    Got rid of search/list component to keep it same as in lesson

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import RestaurantList from "./src/components/RestaurantList";
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
-    // <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    //   <Text>This is our blank canvas</Text>
-    // </View>
-    <RestaurantList />
+    <>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.searchContainer}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.listContainer}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  safeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    flex: 1,
+  },
+  searchContainer: {
+    backgroundColor: "green",
+    padding: 16,
+  },
+  listContainer: {
+    flex: 1,
+    backgroundColor: "blue",
+    padding: 16,
+  },
+});
