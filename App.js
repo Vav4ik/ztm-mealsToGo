5e9bee4697cc0458c63e317ac580ac30a6a7848a
Vav4ik <wawaysms@gmail.com>
2024-01-01 18:30:16 +0000

    Created Safe Area and ReastaurantList component with weird colors (Exercize)

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import RestaurantList from "./src/components/RestaurantList";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>This is our blank canvas</Text>
-    </View>
+    // <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    //   <Text>This is our blank canvas</Text>
+    // </View>
+    <RestaurantList />
   );
 }
 
